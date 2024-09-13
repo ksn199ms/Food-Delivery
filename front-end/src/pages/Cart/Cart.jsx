c5281fdcd8049f1f4155bd31c481a6369239f6eb
ksn199ms <mubashirpannur123@gmail.com>
2024-09-13 14:24:20 +0530

    Front-End is done (some patch work required)

--- front-end/src/pages/Cart/Cart.jsx
+++ front-end/src/pages/Cart/Cart.jsx
@@ -56,12 +56,12 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${2}</p>
+              <p>${getTotalCartAmount() > 0 ? 2 : 0}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount()+2}</b>
+              <b>${getTotalCartAmount() > 0 ? getTotalCartAmount()+2 : 0}</b>
             </div>
           </div>
           <button onClick={() => navigate("/order")}>PROCEED TO CHECKOUT</button>
