c59bf3e749253e706e6133de477952532cbcd2e6
ksn199ms <mubashirpannur123@gmail.com>
2024-09-15 17:54:47 +0530

    Type error fixed

--- back-end/routes/cartRoute.js
+++ back-end/routes/cartRoute.js
@@ -7,7 +7,7 @@ const cartRouter = express.Router();
 
 cartRouter.post('/add',authMiddleware, addToCart)
 cartRouter.post('/remove',authMiddleware, removeFromCart)
-cartRouter.get('/get',authMiddleware, getCart)
+cartRouter.post('/get',authMiddleware, getCart)
 
 
 export default cartRouter
