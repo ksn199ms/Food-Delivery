0b1a0ba59e28fd2fdf20a2f2535fdd805f3942f2
ksn199ms <mubashirpannur123@gmail.com>
2024-09-15 10:26:45 +0530

    URL defined in one place

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -12,6 +12,9 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
+
+  const url = 'http://localhost:4000'
+
   return (
     <div>
       <ToastContainer />
@@ -20,9 +23,9 @@ const App = () => {
       <div className="app-content">
         <Sidebar/>
         <Routes>
-          <Route path='/add' element={<Add/>}/>
-          <Route path='/list' element={<List/>}/>
-          <Route path='/orders' element={<Orders/>}/>
+          <Route path='/add' element={<Add url={url}/>}/>
+          <Route path='/list' element={<List url={url}/>}/>
+          <Route path='/orders' element={<Orders url={url}/>}/>
         </Routes>
       </div>
     </div>
