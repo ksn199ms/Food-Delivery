1154ab5d9eddfcdada2446bef84a0017ecd9f2fa
ksn199ms <mubashirpannur123@gmail.com>
2024-09-16 22:02:22 +0530

    Front-end verify payment functionality integreated

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import PlaceOrder from './pages/PlaceOrder/Placeorder'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 
 import { Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer/Footer'
+import Verify from './pages/Verify/Verify'
 
 const App = () => {
 
@@ -21,6 +22,7 @@ const App = () => {
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/order' element={<PlaceOrder/>}/>
+        <Route path='/verify' element={<Verify/>}/>
       </Routes>
     </div>
     <Footer/>
---
+++ front-end/src/pages/Verify/Verify.jsx
@@ -0,0 +1,41 @@
+import React, { useContext, useEffect } from 'react'
+import './Verify.css'
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
+
+const Verify = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const success = searchParams.get('success')
+    const orderId = searchParams.get('orderId')
+
+    const {url} = useContext(StoreContext)
+
+    const navigate = useNavigate()
+
+    const verifyPayment = async () => {
+        let response = await axios.post(url+"/api/order/verify", {success,orderId})
+        if(response.data.success){
+            navigate('/myorders')
+        }else{
+            alert(response.data.message)
+            navigate('/')
+        }
+    }
+
+
+
+    useEffect(() => {
+        verifyPayment()
+    },[])
+
+  return (
+    <div className='verify'>
+        <div className="spinner"></div>
+    </div>
+  )
+}
+
+export default Verify
