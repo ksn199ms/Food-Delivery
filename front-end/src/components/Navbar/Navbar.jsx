79433b85497dd8a8a6a27bc7fca33649b86ed0d8
ksn199ms <mubashirpannur123@gmail.com>
2024-09-17 15:00:16 +0530

    Profile order navigation added

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = ({setShowLogin}) => {
             : <div className='navbar-profile'>
                 <img src={assets.profile_icon} alt="" />
                 <ul className="nav-profile-dropdown">
-                    <li><img src={assets.bag_icon} alt="" /> <p>Orders</p></li>
+                    <li><img src={assets.bag_icon} alt="" /> <p onClick={() => navigate("/myorders")} >Orders</p></li>
                     <hr />
                     <li onClick={logout}><img src={assets.logout_icon} alt="" /> <p>Logout</p></li>
                 </ul>
