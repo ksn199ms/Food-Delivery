1170c1014e1b9f18f35923780a7f0a88675b8994
ksn199ms <mubashirpannur123@gmail.com>
2024-09-17 14:43:17 +0530

    User orders API call created

--- back-end/controllers/orderController.js
+++ back-end/controllers/orderController.js
@@ -141,4 +141,15 @@ const verifyPayment = async (req, res) => {
     }
 }
 
-export { placeOrder , verifyPayment };
+// user orders for front-end
+const userOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({ userId: req.body.userId });
+        res.json({ success: true, data : orders });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error in fetching orders" });
+    }
+}
+
+export { placeOrder , verifyPayment , userOrders };
--- back-end/routes/orderRoute.js
+++ back-end/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { placeOrder, verifyPayment} from '../controllers/orderController.js'
+import { placeOrder, verifyPayment, userOrders } from '../controllers/orderController.js'
 import authMiddleware from '../middleware/Auth.js';
 
 const orderRouter = express.Router();
@@ -7,6 +7,7 @@ const orderRouter = express.Router();
 
 orderRouter.post('/place',authMiddleware, placeOrder)
 orderRouter.post('/verify', verifyPayment)
+orderRouter.post('/userorders',authMiddleware, userOrders)
 
 
 export default orderRouter
