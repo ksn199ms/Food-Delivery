be9f6476960c84f16afddd63d43627ea4acedf78
ksn199ms <mubashirpannur123@gmail.com>
2024-09-17 14:53:59 +0530

    If the user is available then only he can go to the order page (Logic Added)

--- front-end/src/pages/PlaceOrder/Placeorder.jsx
+++ front-end/src/pages/PlaceOrder/Placeorder.jsx
@@ -1,9 +1,10 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Placeorder.css'
 
 
 import { StoreContext } from '../../context/StoreContext'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Placeorder = () => {
 
@@ -26,31 +27,6 @@ const Placeorder = () => {
     const value = e.target.value
     setData({ ...data, [name]: value })
   }
-
-  // const onPlaceOrder = async (e) => {
-  //   e.preventDefault()
-
-  //   let orderItems = []
-  //   food_list.map((item) => {
-  //     if (cartItems[item._id] > 0) {
-  //       let itemInfo = item;
-  //       itemInfo["quantity"] = cartItems[item._id];
-  //       orderItems.push(itemInfo)
-  //     }
-  //   })
-  //   let orderData = {
-  //     address : data,
-  //     items : orderItems,
-  //     amount : getTotalCartAmount()+2
-  //   }
-  //   let response = await axios.post(url+"/api/order/place",orderData, {headers : {token}})
-  //   if(response.data.success){
-  //     const {session_url} = response.data;
-  //     window.location.replace(session_url);
-  //   }else{
-  //     alert(response.data.message)
-  //   }
-  // }
 
 
   const onPlaceOrder = async (e) => {
@@ -117,6 +93,16 @@ const Placeorder = () => {
       alert("There was an issue placing your order.");
     }
   };
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if(!token){
+      navigate('/cart')
+    }else if(getTotalCartAmount() === 0){
+      navigate('/cart')
+    }
+  },[token])
   
 
   return (
