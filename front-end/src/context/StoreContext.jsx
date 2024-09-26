b29d66523d0b04dc683c1b8d1375ff3b234ffd6b
ksn199ms <mubashirpannur123@gmail.com>
2024-09-26 22:03:01 +0530

    Front-end url replaced

--- front-end/src/context/StoreContext.jsx
+++ front-end/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
 
-    const url = 'http://localhost:4000'
+    const url = 'https://food-delivery-back-end-iy5y.onrender.com'
 
     const [token, setToken] = useState("")
 
