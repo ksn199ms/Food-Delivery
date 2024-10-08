213231e6b351dadfabbbdf0c1f059e2c7e30afd2
ksn199ms <mubashirpannur123@gmail.com>
2024-10-08 17:47:09 +0530

    button action given and transition added

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="header-contents">
             <h2>Order your favorite food here</h2>
             <p> when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            <button>View Menu</button>
+            <button onClick={() => window.location.href = "#explore-menu"} className='btn'>View Menu</button>
         </div>
     </div>
   )
