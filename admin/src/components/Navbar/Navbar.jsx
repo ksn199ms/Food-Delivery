74a26238231a79959c1c67a0b8857694218a1406
ksn199ms <mubashirpannur123@gmail.com>
2024-10-12 16:56:41 +0530

    Admin panel logo changed

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,11 @@ import { assets } from '../../assets/assets'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img className='logo' src={assets.logo} alt="" />
+      {/* <img className='logo' src={assets.logo} alt="" /> */}
+      <div>
+      <h1 className='logo'>TastyTrack.</h1>
+      <p className='admin'>Admin Panel</p>
+      </div>
       <img className='profile' src={assets.profile_image} alt="" />
     </div>
   )
